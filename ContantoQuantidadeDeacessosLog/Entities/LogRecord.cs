b61ff0b6d7e446774fdc0820fad3f3d220c530d5
Luidrocha <luidrocha@yahoo.com.br>
2022-01-18 16:29:41 -0300

    Le um arquivo de Log e conta a quantidad de Acessos - HashSet e Iguals

---
+++ ContantoQuantidadeDeacessosLog/Entities/LogRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContantoQuantidadeDeacessosLog.Entities
+{
+    class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Data { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord reg = obj as LogRecord;
+
+            return UserName.Equals(reg.UserName);
+        }
+    }
+}
---
+++ ContantoQuantidadeDeacessosLog/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ContantoQuantidadeDeacessosLog.Entities;
+
+namespace ContantoQuantidadeDeacessosLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> lr = new HashSet<LogRecord>();
+
+            string sourcePath = @"c:\temp\log.txt"; // Sem o Arroba teria que colocar // no caminho
+
+            // FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                //fs = new FileStream(sourcePath, FileMode.Open); // Abri o arquivo
+                // sr = new StreamReader(fs);
+
+                sr = File.OpenText(sourcePath); // Tem a mesma função do FileStram 
+
+
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(' ');
+                    
+                    string nome = line[0];
+                    DateTime data = DateTime.Parse(line[1]);
+
+                    // Como implementamos o iguals e HashCode na classe por USERNAME, não entra repedido
+                    lr.Add(new LogRecord{ UserName = nome, Data = data });
+
+                }
+                Console.WriteLine("Total de usuario: "+ lr.Count);
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+
+            }
+
+        }
+    }
+}
